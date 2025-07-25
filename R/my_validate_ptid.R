54b2e7af2bb026b923f97dbea88792628dd0bf7a
BenKorea <benkorea.ai@gmail.com>
2025-07-25 20:28:21 +0900

    0725 D

--- R/my_validate_ptid.R
+++
@@ -1,9 +0,0 @@
-my_validate_ptid <- function(dt, col_name) {
-  # 패턴1: 4~8자리 숫자만
-  # 패턴2: T, S, E(대소문자 무관)로 시작하는 값 허용
-  pattern <- "^\\d{4,8}$|^T.*|^S.*|^E.*"
-  
-  invalid_dt <- dt[!grepl(pattern, get(col_name), ignore.case = TRUE), ]
-  
-  return(invalid_dt)
-}
